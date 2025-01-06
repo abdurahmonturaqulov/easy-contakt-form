fe688cc2c5d9cc2d52caabf8d3f3f035ed0ad993
Abdurahmon <rahmlow.1217@mail.ru>
2025-01-06 15:47:32 +0500

    add 3fies

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-const container = document.querySelector(".container");
+//const container = document.querySelector(".container");
 
-const newSquare = document.createElement("div");
-newSquare.classList.add("square");
-newSquare.style.backgroundColor = "blueviolet"
-container.prepend(newSquare);
+//const newSquare = document.createElement("div");
+//newSquare.classList.add("square");
+//newSquare.style.backgroundColor = "blueviolet"
+//container.prepend(newSquare);
 
-const newCircle = document.createElement("div");
-newCircle.classList.add("circle");
-container.prepend(newCircle)
+//const newCircle = document.createElement("div");
+//newCircle.classList.add("circle");
+//container.prepend(newCircle)
+
+//const circle = document.querySelectorAll(".circle");
+//circle[1].after(newSquare)
+
+
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const parag = document.querySelector("p");
+
+btn1.addEventListener("click", addNumber);
+btn2.addEventListener("click", deleteNumber);
+
+
+let startingNumber = 1;
+
+
+
+
+
+function addNumber() {
+  parag.innerText = startingNumber++;
+}
+
+function deleteNumber() {
+  parag.innerText = --startingNumber-1;
+}
