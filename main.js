c0a132b26f8c6d808c5429b8e361573cd9397557
Abdurahmon <rahmlow.1217@mail.ru>
2025-01-07 14:03:09 +0500

    3fileadd

--- main.js
+++ main.js
@@ -1,36 +1,3 @@
-//const container = document.querySelector(".container");
+const todoList = document.querySelector(".todo-list");
+const items = todoList.children;
 
-//const newSquare = document.createElement("div");
-//newSquare.classList.add("square");
-//newSquare.style.backgroundColor = "blueviolet"
-//container.prepend(newSquare);
-
-//const newCircle = document.createElement("div");
-//newCircle.classList.add("circle");
-//container.prepend(newCircle)
-
-//const circle = document.querySelectorAll(".circle");
-//circle[1].after(newSquare)
-
-
-const btn1 = document.querySelector("#btn1");
-const btn2 = document.querySelector("#btn2");
-const parag = document.querySelector("p");
-
-btn1.addEventListener("click", addNumber);
-btn2.addEventListener("click", deleteNumber);
-
-
-let startingNumber = 1;
-
-
-
-
-
-function addNumber() {
-  parag.innerText = startingNumber++;
-}
-
-function deleteNumber() {
-  parag.innerText = --startingNumber-1;
-}
