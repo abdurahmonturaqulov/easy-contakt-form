06022dfb18549a69db60c1430f3373b21e18fd63
Abdurahmon <rahmlow.1217@mail.ru>
2025-01-08 07:41:51 +0500

    adds 3files

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
-const todoList = document.querySelector(".todo-list");
-const items = todoList.children;
+const fName = document.querySelector("form #fName");
+const age = document.querySelector("form #age");
+const isMarried = document.querySelector("form #isMarried");
+
+const fNameText = document.querySelector(".fName");
+const ageText = document.querySelector(".age");
+const isMarriedText = document.querySelector(".isMarried");
+
+const btnSubmit = document.querySelector(".btnSubmit");
+const btnReset = document.querySelector(".btnReset");
+
+const message = document.querySelector(".message");
+
+const containerText = document.querySelector(".container__text");
+
+fName.addEventListener("input", function(e) {
+    fNameText.innerHTML = `Name: ${e.target.value}`;
+});
+age.addEventListener("input", function(e) {
+    ageText.innerHTML = `Age: ${e.target.value}`;
+});
+isMarried.addEventListener("input", function(e) {
+    isMarriedText.innerHTML = `IsMarried: ${e.target.value}`;
+});
+
+btnSubmit.addEventListener("click", function() {
+    message.innerHTML = "Successful send!"
+});
+btnReset.addEventListener("click", function() {
+    containerText.innerHTML = "";
+    message.innerHTML = "Unsuccessful, try again!"
+});
 
